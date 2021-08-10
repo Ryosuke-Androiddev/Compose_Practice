3f0b9ca7ca49f1f5e822a76fc406d2d8b56863ee
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2021-08-10 14:11:36 +0900

    stdied modifier

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,13 +21,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             Row(
                 modifier = Modifier
-                    .width(300.dp)
-                    .fillMaxHeight(0.5f)
-                    .background(Color.LightGray),
+                    .background(Color.LightGray)
+                    .fillMaxSize()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp,Color.Red)
+                    .padding(5.dp)
+                    .border(30.dp,Color.Blue)
+                    .padding(30.dp),
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
             ){
-                Text(text = "My")
+                Text(text = "My",
+                    modifier = Modifier
+                        .offset(5.dp,20.dp)
+                        .border(5.dp,Color.Green)
+                        .padding(5.dp)
+                        .border(5.dp,Color.Black)
+                        .offset(10.dp,40.dp))
                 Text(text = "Name")
                 Text(text = "Ryosuke")
             }
