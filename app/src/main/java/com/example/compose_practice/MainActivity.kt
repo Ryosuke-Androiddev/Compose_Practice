7c471e582d791ad670bb9aa6b8b13e51661d2bbb
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2021-08-10 13:47:04 +0900

    studied column and row

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,31 +3,35 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePracticeTheme {
-                Greeting("Ryosuke")
+            Row(
+                modifier = Modifier
+                    .width(300.dp)
+                    .fillMaxHeight(0.5f)
+                    .background(Color.LightGray),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(text = "My")
+                Text(text = "Name")
+                Text(text = "Ryosuke")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview
-@Composable
-fun DefaultPreview() {
-        Greeting("Android")
-}
